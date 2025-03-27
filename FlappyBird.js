480c73c56ecee54dcb78eeab11efd0723e547dce
Michael <mishakrafter2009@gmail.com>
2025-03-27 17:00:19 +0300

    Big Update FlappyBird.js
    Дописал функцию draw, рисующую все объекты; написал условия, при которых происходит столкновение птички с другими объектами и, если они соблюдаются, то игра заканчивается; сделал функцию, считающую очки.

--- FlappyBird.js
+++ FlappyBird.js
@@ -13,8 +13,6 @@ fg.src = "https://itproger.com/img/news/flappy_bird_fg.png";
 pipeUp.src = "https://itproger.com/img/news/flappy_bird_pipeUp.png";
 pipeBottom.src = "https://itproger.com/img/news/flappy_bird_pipeBottom.png";
 
-const gap = 90;
-
 document.addEventListener("keydown", moveUp);
 
 function moveUp() {
@@ -22,7 +20,6 @@ function moveUp() {
 }
 
 const pipe = [];
-
 pipe[0] = {
     x : cvs.width,
     y : 0
@@ -30,19 +27,46 @@ pipe[0] = {
 
 let score = 0;
 
-let xPos = 10;
-let yPos = 150;
-
+const gap = 100;
 const grav = 1.5;
 
+let xPos = 10;
+let yPos = 150;
 
 function draw() {
     ctx.drawImage(bg, 0, 0);
 
     for (let i = 0; i < pipe.length; i++) {
+        
         ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
         ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
         pipe[i].x--;
+        
+        if(pipe[i].x === 100) {
+            
+            pipe.push({
+                x : cvs.width,
+                y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+            });
+            
+        }
+
+        if(xPos + bird.width >= pipe[i].x
+            && xPos <= pipe[i].x + pipeUp.width
+            && (yPos <= pipe[i].y + pipeUp.height
+            || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
+
+            location.reload();
+
+        }
+
+        if(pipe[i].x === 5) {
+            
+            score++;
+            
+        }
     }
 }
+
+pipeBottom.onload = draw;
